63458c121638074036dc880f1311daa93aa05c74
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-25 15:11:40 +0100

    Initial commit

---
+++ src/main/java/com/demo/javavueapplication/JavaVueApplication.java
@@ -0,0 +1,13 @@
+package com.demo.javavueapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/javavueapplication/JavaVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.javavueapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
